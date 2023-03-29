d20722d1bbac25973817b264f0842ed537a15331
Isaac Perk <97003977+isaacperk@users.noreply.github.com>
2023-03-28 18:12:21 -0700

    prompts and basic inquirer logic added

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const mysql = require("mysql2");
+const inquirer = require("inquirer");
 require("dotenv").config();
+const {
+  promptStart,
+  addDepartmentPrompts,
+  addRolePrompts,
+  addEmployeePrompts,
+} = require("./logic/prompts.js");
 
 const db = mysql.createConnection(
   {
@@ -10,3 +17,21 @@ const db = mysql.createConnection(
   },
   console.log(`Connected to database`)
 );
+
+inquirer.prompt(promptStart).then((response) => {
+  switch (response) {
+    case "View all Departments":
+      db.query("SELECT * FROM departments", function (err, results) {
+        console.log(results);
+      });
+      break;
+    case "View all Roles":
+    case "View all Employees":
+    case "Add a Department":
+    case "Add a Role":
+    case "Add an Employee":
+    case "Update an Employee Role":
+    default:
+      console.log("Sorry there was an error please try again");
+  }
+});
--- logic/prompts.js
+++ logic/prompts.js
@@ -1,7 +1,7 @@
-const promptsMain = [
+const promptStart = [
   {
-    type: list,
-    name: "allOptions",
+    type: "list",
+    name: "promptStart",
     message: "What would you like to do?",
     choices: [
       "View all Departments",
@@ -17,7 +17,7 @@ const promptsMain = [
 
 const addDepartmentPrompts = [
   {
-    type: input,
+    type: "input",
     name: "addDeptName",
     message: "Department Name:",
   },
@@ -25,17 +25,17 @@ const addDepartmentPrompts = [
 
 const addRolePrompts = [
   {
-    type: input,
+    type: "input",
     name: "addRoleName",
     message: "Name of Role:",
   },
   {
-    type: input,
+    type: "input",
     name: "addRoleSalary",
     message: "Salary of Role:",
   },
   {
-    type: input,
+    type: "input",
     name: "addRoleDept",
     message: "Department of Role:",
   },
@@ -43,23 +43,28 @@ const addRolePrompts = [
 
 const addEmployeePrompts = [
   {
-    type: input,
+    type: "input",
     name: "addEmpFirst",
     message: "First Name:",
   },
   {
-    type: input,
+    type: "input",
     name: "addEmpLast",
     message: "Last Name:",
   },
   {
-    type: input,
+    type: "input",
     name: "addEmpRole",
     message: "Role at Company:",
   },
   {
-    type: input,
+    type: "input",
     name: "addEmpMngr",
     message: "Manager:",
   },
 ];
+
+exports.promptStart = promptStart;
+exports.addDepartmentPrompts = addDepartmentPrompts;
+exports.addRolePrompts = addRolePrompts;
+exports.addEmployeePrompts = addEmployeePrompts;
